e4b4e551345203db132de73eb089a250b8258e9e
hujinCN <97722852+hujinCN@users.noreply.github.com>
2025-11-03 13:43:46 +0000

    Deploying to gh-pages from @ hujinCN/hujinCN.github.io@6128050e52f0e63035d2997118d3122b97241cb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-about",
-          title: "About",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/about/";
-          },
-        },{id: "books-the-godfather",
+  },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
